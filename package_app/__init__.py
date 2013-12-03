8e64c3a7795f50541a17a7dd4d1a8014b9b030d9
Phil Schleihauf <uniphil@gmail.com>
2013-12-03 13:11:59 -0500

    package app using send_send_file operating as expected

--- package_app/__init__.py
+++ package_app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory
+import os
+from flask import Flask, send_from_directory, send_file
 
 app = Flask(__name__)
 
@@ -9,3 +10,8 @@ def hello_package():
 @app.route('/from-up-one')
 def hello_package2():
     return send_from_directory('package_app/package_sendfrom_dir', 'sendme_package.txt')
+
+@app.route('/sendfile-nosafejoin')
+def hello_package3():
+    path = os.path.join('package_sendfrom_dir', 'sendme_package.txt')
+    return send_file(path)
